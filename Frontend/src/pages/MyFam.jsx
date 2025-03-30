257294f034a0af0862f406884c7fce7ff2496e61
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2025-03-30 18:50:55 +0530

    some change

--- Frontend/src/pages/MyFam.jsx
+++ Frontend/src/pages/MyFam.jsx
@@ -5,9 +5,14 @@ function MyFam() {
     const {user} = useAuth();
     
   return (
-    <div>
-        
-    </div>
+         <div className='flex flex-col items-center p-4 relative'>
+      <button 
+        className="flex items-center px-4 py-2 bg-gray-100 rounded-lg shadow-sm text-black text-lg font-medium" 
+        onClick={handleChange}
+      >
+        Add a Family Member <span className="ml-2 text-2xl">+</span>
+      </button>
+      </div>
   )
 }
 
