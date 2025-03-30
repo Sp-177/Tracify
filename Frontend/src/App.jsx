348557eccdaced327146b6d805793ef090591c03
siddhanttomar2003 <siddhanttomar2003@gmail.com>
2025-03-30 17:46:25 +0530

    Sign Up / Sign In added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import Map from './Map_Files/Map';
-
-function App() {
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from "react-router-dom";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import MyFam from "./pages/MyFam";
+const App = () => {
   return (
-    <Map/>
-  )
-}
+    <Router>
+      <Routes>
+        <Route path="/myfam" element={<MyFam/>} />
+        <Route path="/signin" element={<SignIn/>} />
+        <Route path="/signup" element={<SignUp/>} />
+      </Routes>
+    </Router>
+  );
+};
 
-export default App; 
+export default App;
---
+++ Frontend/src/context/AuthContext.jsx
@@ -0,0 +1,67 @@
+import { createContext, useContext, useState, useEffect } from "react";
+import toast from 'react-hot-toast';
+import axios from 'axios'
+import { Backendurl } from "../../Private/backend";
+export const AuthContext = createContext({});
+export const AuthProvider = ({ children }) => {
+
+    const [token, setToken] = useState(localStorage.getItem('accessToken') || '');
+    const [user, setUser] = useState("");
+    const [loading, setLoading] = useState(true);  
+    const storeTokenInLS = (accessToken) => {
+        localStorage.setItem('accessToken', accessToken);
+        setToken(accessToken);
+    }
+    let isLoggedIn = !!token;
+
+    const logout = () => {
+        localStorage.removeItem('accessToken');
+        setUser("");
+        setToken("");
+    }
+    
+    const userAuthentication = async () => {
+        setLoading(true);
+        try {
+            const response = await axios.get(`${Backendurl}/api/v1/users/current-user`, {
+                headers: {
+                    Authorization: `Bearer ${token}`,
+                },
+                withCredentials: true,
+            });
+    
+            if (response.status === 200) {
+                console.log("user is ", response.data.data);
+                const userData = response.data.data;
+                setUser(userData);
+                toast.success("User Fetched Succesfully !!");
+            } else {
+                setUser(null);
+            }
+        } catch (error) {
+            console.error("Error in user authentication:", error);
+        
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        if(token){
+            userAuthentication();
+        }
+        else{
+            setLoading(false);
+        }
+    }, [token]);
+    
+    return (
+        <AuthContext.Provider value={{ storeTokenInLS, isLoggedIn, logout, user, loading, token }}>
+            {children}
+        </AuthContext.Provider>
+    )
+}
+
+export const useAuth = ()=>{
+    return useContext(AuthContext);
+}
