1623fc6b3a50caf345b0e98bddc0271760a878ce
ujjwalagrawal-1 <kumarujjwalagrawal4@gmail.com>
2025-04-01 23:11:38 +0530

    Routes Added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,17 +5,17 @@ import SignUp from "./pages/SignUp";
 import Mapfile from "./Map/Mapfile";
 import AboutUs from "./ABOUTUS/AboutUs";
 import { useAuth } from "./context/AuthContext";
-import Mainsection from "./Components/Mainsection";
 
 // Private Route Component
 const PrivateRoute = ({ element }) => {
   const { user } = useAuth();
-  return user ? element : <Navigate to="/signin" />;
+  return user && isLoggedIn ? element : <Navigate to="/signin" />;
 };
 
 import Home from "./pages/Home";
 import ContactPage from "./pages/ContactPage";
 import Profile from "./pages/Profile";
+import MyFam from "./pages/MyFam";
 const App = () => {
   const { user, loading } = useAuth();
 
@@ -26,26 +26,16 @@ const App = () => {
   return (
     <Router>
       <Routes>
-
-        <Route path="/" element={<Mainsection/>} />
+        <Route path="/" element={<Home />} />
         <Route path="/aboutus" element={<AboutUs />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
-        {user ? (
-          <Route path="/maparea" element={<Mapfile userId={user._id} familyId={user.familyId} />} />
-        ) : (
-          <Route path="/maparea" element={<SignIn />} />
-        )}
-        <Route path="/signin" element={<SignIn/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="/" element={<Home />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/map" element={<Mapfile />} />
         <Route path="/profile" element={<Profile />} />
-
         {/* Protected Routes - Only accessible if user is logged in */}
         <Route path="/myfam" element={<PrivateRoute element={<MyFam />} />} />
-        <Route path="/maparea" element={<PrivateRoute element={<Mapfile userId={user?._id} familyId={user?.familyId} />} />} />
+        <Route path="/maparea" element={<PrivateRoute element={<Mapfile userId={user._id} familyId={user.familyId} />} />} />
       </Routes>
     </Router>
   );
